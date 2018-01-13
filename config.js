2f8a4f64316469cea014d6a846a6201bfeb9aaeb
Yyyyaaaannnnoooo <yannpatrickmartins@gmail.com>
2018-01-13 15:44:45 +0100

    update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-      apiKey: "AIzaSyCbAxda4yvrLEdjR3fcVgbU9ms04ek7tNI",
-      authDomain: "dither-b578e.firebaseapp.com",
-      databaseURL: "https://dither-b578e.firebaseio.com",
-      projectId: "dither-b578e",
+      apiKey: "",
+      authDomain: "",
+      databaseURL: "",
+      projectId: "",
       storageBucket: "",
-      messagingSenderId: "827206455788"
+      messagingSenderId: ""
     };
